3e432ed4bbc2849ca1bcd9eb9e6d3bf4b58062df
Santiago Nunez-Corrales <nunezco2@illinois.edu>
2022-05-24 09:37:18 -0500

    Update abstract voter model

--- csssa2022/abstractvotermodel.py
+++ csssa2022/abstractvotermodel.py
@@ -4,48 +4,67 @@
 # terms of the Mozilla Public License v2.0 which accompanies this distribution,
 # and is available at https://www.mozilla.org/en-US/MPL/2.0/
 import random
+import math
 
+from abc import ABC, abstractmethod
+from csssa2022.database import Database
 
-class AbstractVoterModel:
+class AbstractVoterModel(ABC):
     
-    def __init__(self, type, interactions, interactants, initial_state,
-                 network, n, gamma, ensemble, max_steps):
+    def __init__(self, uuid, ensemble_id, type, interactions, interactants,
+                 initial_state, network, n, gamma, max_steps, db: Database):
         # General elements
         self.running = True
         self.stepno = 0
         self.max_steps = max_steps
         
+        # Model id
+        self.uuid = uuid
+        
         # Voter specific attributes
         self.type = type
         self.interactions = interactions
         self.interactants = interactants
+        self.initial_state = initial_state
         self.network = network
         self.n = n
         self.gamma = gamma
-        self.ensemble = ensemble
+        self.ensemble_id = ensemble_id
         
-        # Create agents and store their opinion as a map
-        self.agents = {}
-        all_agents = list(range(0, self.n))
+        # Create agents, separate them into initial states of yes/no
+        self.agent_list = list(range(0, self.n))
+        self.initial_yes = random.sample(self.agent_list, math.ceil(n*self.initial_state))
+        self.initial_no = list(set(self.agent_list) - set(self.initial_yes))
         
-       
+        # Set the database where to store elements
+        self.db = db
         
     def agents(self):
-        return list(self.agents)
+        return list(self.agent_list)
     
+    @abstractmethod
     def agent_at(self, i):
-        if i in self.agents:
-            return self.agents[i]
-        else:
-            raise ValueError
+        pass
         
+    @abstractmethod
     def step(self):
         pass
     
-    def save(self):
+    @abstractmethod
+    def compute_f(self, i):
         pass
     
-    def run_model(self):
-        if self.running:
-            while self.stepno < self.max_steps:
-                self.step()
+    @abstractmethod
+    def agent_to_record(self, i):
+        pass
+    
+    def save(self, i):
+        self.db.insert(self.agent_to_record(i))
+    
+    def save_all(self):
+        '''
+        Save all takes all agents and, depending on the implementation of agent_to_record,
+        takes care of saving one full iteration. Commit occurs at the end of the simulation
+        '''
+        for i in self.agent_list:
+            self.save(i)
