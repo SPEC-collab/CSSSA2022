a4d78bb41619e0a8fc87ac1ea8447e80cff52ba2
Santiago Nunez-Corrales <nunezco2@illinois.edu>
2022-06-02 11:34:04 -0500

    Implement ABM version of higher order voter model

--- csssa2022/higherorderabmvotermodel.py
+++ csssa2022/higherorderabmvotermodel.py
@@ -3,6 +3,9 @@
 # This program and the accompanying materials are made available under the
 # terms of the Mozilla Public License v2.0 which accompanies this distribution,
 # and is available at https://www.mozilla.org/en-US/MPL/2.0/
+import math
+import random
+
 from networkx import Graph
 from csssa2022.selections import InteractionType, SimulationType
 from csssa2022.abstractvotermodel import AbstractVoterModel
@@ -11,18 +14,50 @@ from mesa.time import RandomActivation
 
 
 class HigherOrderABMVoterAgent(Agent):
-    def __init__(self, unique_id: int, initial_opinion:int, model: Model):
+    def __init__(self, unique_id: int, initial_opinion:int, model):
         super().__init__(unique_id, model)
+        # Agents are not active unless the scheduler activates them during each model step
+        self.active = False
         self.opinion = initial_opinion
         self.f = 0
         
     def step(self):
-        '''
-        Higher-order interactions are controlled by the scheduler a the top
-        `step` function within the main model.
-        '''
-        pass
+        if self.active:
+            # Partition the graph based on current id
+            partition = self.model.get_neighbors(self.unique_id) + [self.unique_id]
+                
+            # Compute the value of the partition
+            f_part, interactants = self.compute_f(partition, self.model.interactants)
+            
+            if f_part > self.model.f_threshold:
+                self.propagate(interactants, 1, f_part)
+            else:
+                self.propagate(interactants, 0, f_part)
+            
+            self.active = False
+            
+    def compute_f(self, partition, interactants):
+        total = 0.0
     
+        k = len(partition)
+        
+        # Take a subset of interactants when possible
+        if k > interactants:
+            sample = random.sample(partition, interactants)
+            k = interactants
+        else:
+            sample = partition
+        
+        for j in sample:
+            total += self.model.get_opinion(j)
+            
+        total /= k
+        return total, sample
+    
+    def propagate(self, interactants, opinion, f):
+        for i in interactants:
+            self.model.get_agent(i).opinion = opinion
+            self.model.get_agent(i).f = f
         
 class HigherOrderABMVoterModel(AbstractVoterModel,Model):
     '''
@@ -51,22 +86,30 @@ class HigherOrderABMVoterModel(AbstractVoterModel,Model):
         for i in self.initial_no:
             no_agent = HigherOrderABMVoterAgent(i, 0, self)
             self.schedule.add(no_agent)
-
+            
     def step(self):
         if self.stepno == self.max_steps:
             self.running = False
         else:
-            # TODO: implement the higher-order interaction process
+           # Obtain a random sample set corresponding to centroids
+           centroids = random.sample(self.agent_list, math.ceil(self.n/self.interactants))
+           
+           # Activate only these agents
+           for a in filter(lambda a: a.unique_id in centroids, self.schedule.agents):
+               a.active = True
+            
            self.schedule.step()
            
     def compute_f(self, i):
-        # TODO: perform higher order interactions here.
         pass
     
     def get_opinion(self, i):
-        filtered = list(filter(lambda a: a.unique_id == i, self.schedule.agents))
-        return filtered[0].opinion
+        return self.get_agent(i).opinion
     
     def get_f(self, i):
         filtered = list(filter(lambda a: a.unique_id == i, self.schedule.agents))
         return filtered[0].f
+    
+    def get_agent(self, i):
+        filtered = list(filter(lambda a: a.unique_id == i, self.schedule.agents))
+        return filtered[0]
