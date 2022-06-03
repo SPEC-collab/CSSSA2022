37c1714587258d86d1ec1f7bcb22a321e25b3a19
Santiago Nunez-Corrales <nunezco2@illinois.edu>
2022-06-03 18:09:18 -0500

    Correct bash script

--- model_runner.bash
+++ model_runner.bash
@@ -10,11 +10,11 @@ DB_NAME="csssa2022.db"
 
 # Run all combinations of models with a single database
 
-python main.py matrix   dyn     l2dr    $(INTERACTANTS) $(N) $(MAX_STEPS) $(ENSEMBLE_SIZE) $(INITIAL_MAG) $(DB_NAME)
-python main.py matrix   dyn     pl      $(INTERACTANTS) $(N) $(MAX_STEPS) $(ENSEMBLE_SIZE) $(INITIAL_MAG) $(DB_NAME)
-python main.py matrix   hord    l2dr    $(INTERACTANTS) $(N) $(MAX_STEPS) $(ENSEMBLE_SIZE) $(INITIAL_MAG) $(DB_NAME)
-python main.py matrix   hord    pl      $(INTERACTANTS) $(N) $(MAX_STEPS) $(ENSEMBLE_SIZE) $(INITIAL_MAG) $(DB_NAME)
-python main.py abm      dyn     l2dr    $(INTERACTANTS) $(N) $(MAX_STEPS) $(ENSEMBLE_SIZE) $(INITIAL_MAG) $(DB_NAME)
-python main.py abm      dyn     pl      $(INTERACTANTS) $(N) $(MAX_STEPS) $(ENSEMBLE_SIZE) $(INITIAL_MAG) $(DB_NAME)
-python main.py abm      hord    l2dr    $(INTERACTANTS) $(N) $(MAX_STEPS) $(ENSEMBLE_SIZE) $(INITIAL_MAG) $(DB_NAME)
-python main.py abm      hord    pl      $(INTERACTANTS) $(N) $(MAX_STEPS) $(ENSEMBLE_SIZE) $(INITIAL_MAG) $(DB_NAME)
+python main.py matrix   dyn     l2dr    $INTERACTANTS $N $MAX_STEPS $ENSEMBLE_SIZE $INITIAL_MAG $DB_NAME
+python main.py matrix   dyn     pl      $INTERACTANTS $N $MAX_STEPS $ENSEMBLE_SIZE $INITIAL_MAG $DB_NAME
+python main.py matrix   hord    l2dr    $INTERACTANTS $N $MAX_STEPS $ENSEMBLE_SIZE $INITIAL_MAG $DB_NAME
+python main.py matrix   hord    pl      $INTERACTANTS $N $MAX_STEPS $ENSEMBLE_SIZE $INITIAL_MAG $DB_NAME
+python main.py abm      dyn     l2dr    $INTERACTANTS $N $MAX_STEPS $ENSEMBLE_SIZE $INITIAL_MAG $DB_NAME
+python main.py abm      dyn     pl      $INTERACTANTS $N $MAX_STEPS $ENSEMBLE_SIZE $INITIAL_MAG $DB_NAME
+python main.py abm      hord    l2dr    $INTERACTANTS $N $MAX_STEPS $ENSEMBLE_SIZE $INITIAL_MAG $DB_NAME
+python main.py abm      hord    pl      $INTERACTANTS $N $MAX_STEPS $ENSEMBLE_SIZE $INITIAL_MAG $DB_NAME
