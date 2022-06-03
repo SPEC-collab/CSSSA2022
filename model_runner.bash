5ebba521eb6b17e75546d9cd977dd8cc0999557b
Santiago Nunez-Corrales <nunezco2@illinois.edu>
2022-06-03 18:04:18 -0500

    Add bash model runner

---
+++ model_runner.bash
@@ -0,0 +1,20 @@
+#/bin/bash
+
+INTERACTANTS=3
+N=256
+MAX_STEPS=2000
+ENSEMBLE_SIZE=50
+INITIAL_MAG=0.3
+DB_NAME="csssa2022.db"
+
+
+# Run all combinations of models with a single database
+
+python main.py matrix   dyn     l2dr    $(INTERACTANTS) $(N) $(MAX_STEPS) $(ENSEMBLE_SIZE) $(INITIAL_MAG) $(DB_NAME)
+python main.py matrix   dyn     pl      $(INTERACTANTS) $(N) $(MAX_STEPS) $(ENSEMBLE_SIZE) $(INITIAL_MAG) $(DB_NAME)
+python main.py matrix   hord    l2dr    $(INTERACTANTS) $(N) $(MAX_STEPS) $(ENSEMBLE_SIZE) $(INITIAL_MAG) $(DB_NAME)
+python main.py matrix   hord    pl      $(INTERACTANTS) $(N) $(MAX_STEPS) $(ENSEMBLE_SIZE) $(INITIAL_MAG) $(DB_NAME)
+python main.py abm      dyn     l2dr    $(INTERACTANTS) $(N) $(MAX_STEPS) $(ENSEMBLE_SIZE) $(INITIAL_MAG) $(DB_NAME)
+python main.py abm      dyn     pl      $(INTERACTANTS) $(N) $(MAX_STEPS) $(ENSEMBLE_SIZE) $(INITIAL_MAG) $(DB_NAME)
+python main.py abm      hord    l2dr    $(INTERACTANTS) $(N) $(MAX_STEPS) $(ENSEMBLE_SIZE) $(INITIAL_MAG) $(DB_NAME)
+python main.py abm      hord    pl      $(INTERACTANTS) $(N) $(MAX_STEPS) $(ENSEMBLE_SIZE) $(INITIAL_MAG) $(DB_NAME)
